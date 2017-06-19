3699488a8d30e44f6909a615288512132d990e35
leanhkhoi <leanhkhoi1996@gmail.com>
2017-06-19 15:09:07 +0700

    update form tìm kiếm

--- DANodejs/controllers/productController.js
+++ DANodejs/controllers/productController.js
@@ -1,10 +1,13 @@
 var express = require('express'),
-    productRepo = require('../models/productRepo');
+    productRepo = require('../models/productRepo'),
+    categoryRepo = require('../models/categoryRepo'),
+    userRepo = require('../models/userRepo');
 
 var activeCategoty;
+var activePro;
 var r = express.Router();
 
-    r.get('/bycat/:id', function(req, res) {
+r.get('/bycat/:id', function(req, res) {
 
         var catId = req.params.id;
         activeCategoty = catId;
@@ -23,16 +26,97 @@ var r = express.Router();
                 };
                 res.render('product/bycat', vm);
             });
-    });
+});
 
-
-    r.post('/timkiem', function(req, res) {
-         productRepo.search(req.body)
+r.post('/bycat/:id', function(req, res) {
+        var catId = req.params.id;
+        if (!catId) {
+            res.redirect('/');
+        }
+        if(req.body.loaibtn==='1'){
+            
+            productRepo.sapxep_thoigian_loai_khongten(catId)
             .then(function(pRows) {
                 var vm = {
                     layoutVM: res.locals.layoutVM,
                     products: pRows,
-                    ten: req.body.productName,
+                    activeCat : activeCategoty,
+                    noProducts: pRows.length === 0
+                };
+                res.render('product/bycat', vm);
+            });
+        
+        }
+        else
+        {
+              productRepo.sapxep_gia_loai_khongten(catId)
+            .then(function(pRows) {
+                var vm = {
+                    layoutVM: res.locals.layoutVM,
+                    products: pRows,
+                    activeCat : activeCategoty,
+                    noProducts: pRows.length === 0
+                };
+                res.render('product/bycat', vm);
+            });
+        }
+       
+});
+r.get('/chitietsanpham/:id', function(req, res) {
+      
+        var proId = req.params.id;
+        activePro = proId;
+        if (!proId) {
+            res.redirect('/');
+        }
+       
+        productRepo.loadProductById(proId)
+            .then(function(pro) {
+                     categoryRepo.loadById(pro[0].loai)
+                    .then(function(cat){
+                        userRepo.loadUserById(pro[0].idNguoiBan)
+                        .then(function(seller){
+                            userRepo.loadUserById(pro[0].idNguoiGiaCaoNhat)
+                            .then(function(bestbuyer){
+                                      var vm = {
+                                        layoutVM: res.locals.layoutVM,
+                                        products: pro,
+                                        category: cat[0].tenLoaiSanPham,
+                                        seller : seller,
+                                        bestbuyer : bestbuyer,
+                                        noProduct: pro.length === 0
+                                      };
+                                      res.render('product/ChiTietSanPham', vm);
+                            });
+                           
+                        });
+                       
+                    });
+
+        });
+        /*productRepo.loadProductById(proId)
+             .then(function(pro) {
+                     
+                     var vm = {
+                            layoutVM: res.locals.layoutVM,
+                            product: pro,
+                            active : pro[0].idSanPham,
+                            noProduct: pro.length === 0
+                        };
+                    res.render('product/ChiTietSanPham', vm);
+                });*/
+
+
+});
+
+
+r.get('/timkiem', function(req, res) {
+         productRepo.search(req.query.ten)
+            .then(function(pRows) {
+                var vm = {
+                    layoutVM: res.locals.layoutVM,
+                    products: pRows,
+                    ten: req.query.ten,
                     id: -1,
                     noProducts: pRows.length === 0
                 };
@@ -40,14 +124,14 @@ var r = express.Router();
             });
        
 });
-r.post('/timkiemtheoloai', function(req, res) {
-         productRepo.search_loai(req.body)
+r.get('/timkiemtheoloai', function(req, res) {
+         productRepo.search_loai(req.query.id,req.query.ten)
             .then(function(pRows) {
                 var vm = {
                     layoutVM: res.locals.layoutVM,
                     products: pRows,
-                    ten: req.body.productName,
-                    id: req.body.catID,
+                    ten: req.query.ten,
+                    id: req.query.id,
                     noProducts: pRows.length === 0
                 };
                 res.render('product/ketquatimkiem', vm);
@@ -116,38 +200,27 @@ r.post('/sapxep', function(req, res) {
         }
        
 });
-r.post('/bycat/:id', function(req, res) {
-         var catId = req.params.id;
-        if (!catId) {
-            res.redirect('/');
+
+
+
+
+r.get('/favourite/:id', function(req, res){
+    var userId = req.cookies.userLogin;
+     if(userId){
+          var proId = req.params.id;
+           
+            if (!proId) {
+
+                res.redirect('back');
+            }
+           
+           
+            productRepo.insertFavourite(proId,userId)
+                .then(function() {
+                   res.redirect('back'); 
+                });
         }
-        if(req.body.loaibtn==='1'){
-            
-            productRepo.sapxep_thoigian_loai_khongten(catId)
-            .then(function(pRows) {
-                var vm = {
-                    layoutVM: res.locals.layoutVM,
-                    products: pRows,
-                    activeCat : activeCategoty,
-                    noProducts: pRows.length === 0
-                };
-                res.render('product/bycat', vm);
-            });
-        
-        }
-        else
-        {
-              productRepo.sapxep_gia_loai_khongten(catId)
-            .then(function(pRows) {
-                var vm = {
-                    layoutVM: res.locals.layoutVM,
-                    products: pRows,
-                    activeCat : activeCategoty,
-                    noProducts: pRows.length === 0
-                };
-                res.render('product/bycat', vm);
-            });
-        }
-       
+
 });
+
 module.exports = r;
